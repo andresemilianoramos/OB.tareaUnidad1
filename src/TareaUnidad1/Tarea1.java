9126815365e2b5a36ce967aa858234c30c52c42f
andresemiliano1 <andresemiliano1@gmail.com>
2022-12-28 14:51:12 +0100

    Initial commit

---
+++ src/TareaUnidad1/Tarea1.java
@@ -0,0 +1,25 @@
+package TareaUnidad1;
+
+public class Tarea1 {
+    public static void main(String[] args) {
+        byte Byte = 1;
+        short Short = 2;
+        int Int = 3;
+        long Long = 4;
+        float Float = 5;
+        double Double = 6;
+        char Char = 'c';
+        boolean check = true;
+        String string = "string";
+
+        System.out.println(Byte);
+        System.out.println(Short);
+        System.out.println(Int);
+        System.out.println(Long);
+        System.out.println(Float);
+        System.out.println(Double);
+        System.out.println(Char);
+        System.out.println(check);
+        System.out.println(string);
+    }
+}

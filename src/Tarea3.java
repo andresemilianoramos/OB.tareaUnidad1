68dfed11f259bfdf66368327ab9d5225859c41e9
andresemiliano1 <andresemiliano1@gmail.com>
2022-12-28 17:55:12 +0100

    Tarea3

---
+++ src/Tarea3.java
@@ -0,0 +1,10 @@
+public class Tarea3 {
+    public static void main(String[] args) {
+
+        String[] textos = {" El"," dia"," esta"," muy bonito."};
+
+        for (int i = 0; i < textos.length; i++) {
+            System.out.print(textos[i]);
+        }
+    }
+}
